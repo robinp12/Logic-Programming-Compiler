00cbe6269c7b9aebb6b9adf8679cfc77b5ef1038
Robin <robin-be@hotmail.com>
2022-03-07 19:55:53 +0100

    update project base

--- src/norswap/sigh/Test.java
+++ src/norswap/sigh/Test.java
@@ -36,6 +36,7 @@ public final class Test
         walker.walk(tree);
         reactor.run();
 
+
         if (!reactor.errors().isEmpty()) {
             System.out.println(reactor.reportErrors(it ->
                 it.toString() + " (" + ((SighNode) it).span.startString(lineMap) + ")"));
